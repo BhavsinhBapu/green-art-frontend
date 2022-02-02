aaf33dc7e93bb4682141a4e8a5ce5da9d2a1c34e
anirban_itech <anirban@itech-softsolutions.com>
2022-02-02 12:41:44 +0600

    request file setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-
+import Cookies from "js-cookie"
 const Home: NextPage = () => {
+
+
   return <div className={styles.container}>hello</div>;
 };
 
