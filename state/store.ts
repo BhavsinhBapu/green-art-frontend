8cb9db9c8d1361ea22b533a4b57c32b415265382
anirban00537 <anirban00537@gmail.com>
2022-09-16 12:03:28 +0600

    updated store

--- state/store.ts
+++ state/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./reducer/user";
 import { exchangeSlice } from "./reducer/exchange";
+import { commonSlice } from "./reducer/common";
 export const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     exchange: exchangeSlice.reducer,
+    common: commonSlice.reducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
