444c1591231a0fcde916352bf8637d75b7c429a0
anirban00537 <anirban00537@gmail.com>
2022-09-26 18:26:57 +0600

    updated

--- components/exchange/api/historyProvider.ts
+++ components/exchange/api/historyProvider.ts
@@ -1,14 +1,21 @@
-import Cookies from "js-cookie";
-import { getChartData } from "service/trading-chart";
+// import { TV_CHART } from "service/trading-chart";
 import { apiRequest } from "lib/request";
+import { getChartData } from "service/trading-chart";
 const history: any = {};
+const api_root = "https://min-api.cryptocompare.com";
 
 export default {
   history: history,
-  hitted: false,
-
   //@ts-ignore
   getBars: function (symbolInfo, resolution, from, to, first, limit) {
+    const base = localStorage.getItem("current_pair")?.split("_")[0]
+      ? localStorage.getItem("current_pair")?.split("_")[0]
+      : "BTC";
+    const trade = localStorage.getItem("current_pair")?.split("_")[1]
+      ? localStorage.getItem("current_pair")?.split("_")[1]
+      : "USDT";
+    const baseId = localStorage.getItem("base_coin_id");
+    const tradeId = localStorage.getItem("trade_coin_id");
     var split_symbol = symbolInfo.name.split(/[:/]/);
     const url =
       resolution === "D"
@@ -16,32 +23,38 @@ export default {
         : resolution >= 60
         ? "/data/histohour"
         : "/data/histominute";
+    const qs = {
+      e: split_symbol[0],
+      fsym: base,
+      tsym: trade,
+      toTs: to,
+      limit: limit ? limit : 2000,
+      // aggregate: 1//resolution
+    };
+    //time stamp
 
-    const base = localStorage.getItem("base_coin_id");
-    const trade = localStorage.getItem("trade_coin_id");
-
-    this.hitted = true;
-    return getChartData(5, from, to, base ? base : 2, trade ? trade : 1).then(
-      (data: any) => {
-        if (data.data.data.length) {
-          const myBars = data.data.data;
-          const klines4800 = [...myBars, ...myBars, ...myBars];
-          const bars = klines4800.map((el: any) => ({
-            time: el.time * 1000,
-            low: el.low,
-            high: el.high,
-            open: el.open,
-            close: el.close,
-            volume: el.volume,
-          }));
-          if (first) {
-            const lastBar = bars[bars.length - 1];
-            history[symbolInfo.name] = { lastBar };
-          }
-          return bars;
+    return apiRequest(
+      `${api_root}${url}?`,
+      `${qs.e}&fsym=${qs.fsym}&tsym=${qs.tsym}&toTs=${qs.toTs}&limit=${qs.limit}`
+    ).then((data: any) => {
+      if (data.data.Data.length) {
+        const myBars = data.data.Data;
+        const klines4800 = [...myBars, ...myBars, ...myBars];
+        const bars = klines4800.map((el: any) => ({
+          time: el.time * 1000,
+          low: el.low,
+          high: el.high,
+          open: el.open,
+          close: el.close,
+          volume: el.volumefrom,
+        }));
+        if (first) {
+          const lastBar = bars[bars.length - 1];
+          history[symbolInfo.name] = { lastBar };
         }
-        return [];
+        return bars;
       }
-    );
+      return [];
+    });
   },
 };
