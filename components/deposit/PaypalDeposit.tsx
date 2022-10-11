a8cdf79430ca5bcff6e6c1d8a41bf1d8e2fd9ca9
anirban00537 <anirban00537@gmail.com>
2022-10-11 19:16:17 +0600

    env updated

--- components/deposit/PaypalDeposit.tsx
+++ components/deposit/PaypalDeposit.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/router";
 
 const paypalScriptOptions: PayPalScriptOptions = {
   //@ts-ignore
-  "client-id": process.env.NEXT_PUBLIC_PAYPAL_SECRET_KEY,
+  "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_KEY,
   currency: "USD",
 };
 function Button({ credential, setCredential }: any) {
