cacea5a5aa4bc1abe069f07dec995e2b024325c9
anirban00537 <anirban00537@gmail.com>
2022-10-18 18:00:20 +0600

    updated
    Former-commit-id: 6a18c7ccaa61f20447a77d2635161aa458cb720b

--- service/trading-chart.tsx
+++ service/trading-chart.tsx
@@ -8,6 +8,18 @@ export const getChartData = (
   trade: any
 ) => {
   return request({
+    url: `/get-exchange-chart-data-app?base_coin_id=${base}&trade_coin_id=${trade}&interval=${interval}`,
+    method: "GET",
+  });
+};
+export const getChartDataWithoutTime = (
+  interval: any,
+  startTime: any,
+  endTime: any,
+  base: any,
+  trade: any
+) => {
+  return request({
     url: `/get-exchange-chart-data-app?base_coin_id=${base}&trade_coin_id=${trade}&interval=${interval}&start_time=${startTime}&end_time=${endTime}`,
     method: "GET",
   });
