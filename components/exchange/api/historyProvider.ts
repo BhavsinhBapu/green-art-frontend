26d42bbf29abe9ad1f48dc94e29e389cab3afc99
anirban00537 <anirban00537@gmail.com>
2022-10-18 18:00:20 +0600

    updated

--- components/exchange/api/historyProvider.ts
+++ components/exchange/api/historyProvider.ts
@@ -1,4 +1,4 @@
-import { getChartData } from "service/trading-chart";
+import { getChartData, getChartDataWithoutTime } from "service/trading-chart";
 const history: any = {};
 
 export default {
@@ -10,31 +10,61 @@ export default {
     const base = localStorage.getItem("base_coin_id");
     const trade = localStorage.getItem("trade_coin_id");
 
-    this.hitted = true;
-    return getChartData(5, from, to, base, trade).then((data: any) => {
-      if (data.data.data.length) {
-        const myBars = data.data.data;
-        let klines4800 = [...myBars, ...myBars];
-        if (klines4800.length < 320) {
-          for (let i = 0; i < 320; i++) {
-            klines4800 = [...klines4800, ...myBars];
+    if (!this.hitted) {
+      this.hitted = true;
+      return getChartData(5, from, to, base, trade).then((data: any) => {
+        if (data.data.data.length) {
+          const myBars = data.data.data;
+          let klines4800 = [...myBars, ...myBars];
+          if (klines4800.length < 320) {
+            for (let i = 0; i < 320; i++) {
+              klines4800 = [...klines4800, ...myBars];
+            }
           }
+          const bars = klines4800.map((el: any) => ({
+            time: el.time * 1000,
+            low: el.low,
+            high: el.high,
+            open: el.open,
+            close: el.close,
+            volume: el.volume,
+          }));
+          if (first) {
+            const lastBar = bars[bars.length - 1];
+            history[symbolInfo.name] = { lastBar };
+          }
+          return bars;
         }
-        const bars = klines4800.map((el: any) => ({
-          time: el.time * 1000,
-          low: el.low,
-          high: el.high,
-          open: el.open,
-          close: el.close,
-          volume: el.volume,
-        }));
-        if (first) {
-          const lastBar = bars[bars.length - 1];
-          history[symbolInfo.name] = { lastBar };
+        return [];
+      });
+    } else {
+      return getChartDataWithoutTime(5, from, to, base, trade).then(
+        (data: any) => {
+          if (data.data.data.length) {
+            const myBars = data.data.data;
+            let klines4800 = [...myBars, ...myBars];
+            if (klines4800.length < 320) {
+              for (let i = 0; i < 320; i++) {
+                klines4800 = [...klines4800, ...myBars];
+              }
+            }
+            const bars = klines4800.map((el: any) => ({
+              time: el.time * 1000,
+              low: el.low,
+              high: el.high,
+              open: el.open,
+              close: el.close,
+              volume: el.volume,
+            }));
+            if (first) {
+              const lastBar = bars[bars.length - 1];
+              history[symbolInfo.name] = { lastBar };
+            }
+            return bars;
+          }
+          return [];
         }
-        return bars;
-      }
-      return [];
-    });
+      );
+    }
   },
 };
