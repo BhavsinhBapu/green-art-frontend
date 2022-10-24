1fb6dcf344a3536926e24c151cd8b27635d17a05
Naim <naimkst@gmail.com>
2022-10-24 16:33:06 +0600

    Trade dropdown done
    Former-commit-id: c18f39f945862c90721ee261ede271283fa1d66f

---
+++ components/TradeList/index.tsx
@@ -0,0 +1,39 @@
+import router from "next/router";
+import React from "react";
+import { setCurrentPair } from "state/reducer/exchange";
+
+export const TradeList = ({ coinList }: any) => {
+  function dispatch(arg0: any) {
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <div className="tradeList">
+      {coinList?.map((item: any, index: number) => (
+        <div
+          onClick={async () => {
+            await localStorage.setItem("base_coin_id", item?.parent_coin_id);
+            await localStorage.setItem("trade_coin_id", item?.child_coin_id);
+            await localStorage.setItem(
+              "current_pair",
+              item?.child_coin_name + "_" + item?.parent_coin_name
+            );
+          }}
+          key={`coinList${index}`}
+          className="coinList"
+        >
+          <img src={item.icon} alt="" />
+          <a
+            href={
+              router.locale !== "en"
+                ? `/${router.locale}/exchange/dashboard`
+                : "/exchange/dashboard"
+            }
+          >
+            {item.coin_pair_name}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
