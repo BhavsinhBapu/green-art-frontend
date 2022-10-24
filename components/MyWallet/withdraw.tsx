8f23af6d83151a28bc5fa8eeb4ed238d662bc668
Naim <naimkst@gmail.com>
2022-10-24 12:46:14 +0600

    Wallet page separate done

--- components/MyWallet/withdraw.tsx
+++ components/MyWallet/withdraw.tsx
@@ -2,6 +2,7 @@ import useTranslation from "next-translate/useTranslation";
 import React from "react";
 import { copyTextById, formateZert } from "common";
 import WalletGoogleAuth from "components/wallet/wallet-google-auth";
+import { UserSettingsApi } from "service/settings";
 
 export const WithdrawComponent = ({ responseData, router }: any) => {
   const { t } = useTranslation("common");
@@ -21,6 +22,33 @@ export const WithdrawComponent = ({ responseData, router }: any) => {
     status: false,
     message: "",
   });
+
+  const CheckG2faEnabled = async () => {
+    const { data } = await UserSettingsApi();
+    const { user } = data;
+    if (user.google2fa === 1) {
+    } else {
+      setErrorMessage({
+        status: true,
+        message: "Google 2FA is not enabled, Please enable Google 2FA fist",
+      });
+    }
+  };
+  React.useEffect(() => {
+    setWithdrawalCredentials({
+      ...withdrawalCredentials,
+      wallet_id: responseData?.wallet?.id,
+    });
+
+    CheckG2faEnabled();
+  }, [responseData?.wallet?.id]);
+
+  React.useEffect(() => {
+    setWithdrawalCredentials({
+      ...withdrawalCredentials,
+      network_type: selectedNetwork?.network_type,
+    });
+  }, [selectedNetwork?.network_type]);
 
   return (
     <div className={`col-md-7`}>
