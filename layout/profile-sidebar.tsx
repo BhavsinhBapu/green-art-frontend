8f29056472ce6139dddebed54fd32295dfe3354c
anirban00537 <anirban00537@gmail.com>
2022-10-25 14:02:34 +0600

    updated
    Former-commit-id: 2342b2c29f412e23a56dd9d6006ede31b051aaff

--- layout/profile-sidebar.tsx
+++ layout/profile-sidebar.tsx
@@ -1,5 +1,12 @@
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineDomainVerification, MdPassword } from "react-icons/md";
+import {
+  AiTwotoneEdit,
+  AiTwotonePhone,
+  AiFillSecurityScan,
+} from "react-icons/ai";
 import { useRouter } from "next/router";
 const ProfileSidebar = () => {
   const router = useRouter();
@@ -10,6 +17,7 @@ const ProfileSidebar = () => {
         <ul className="left-menu">
           <Link href="/user/profile">
             <li className={router.pathname == "/user/profile" ? "active" : ""}>
+              <CgProfile />
               <a href="/user/profile">{t("Profile")}</a>
             </li>
           </Link>
@@ -19,6 +27,7 @@ const ProfileSidebar = () => {
                 router.pathname == "/user/edit-profile" ? "active" : ""
               }
             >
+              <AiTwotoneEdit />
               <a href="/user/edit-profile">{t("Edit Profile")}</a>
             </li>
           </Link>
@@ -28,11 +37,13 @@ const ProfileSidebar = () => {
                 router.pathname == "/user/phone-verification" ? "active" : ""
               }
             >
+              <AiTwotonePhone />
               <a href="/user/phone-verification">{t("Phone Verification")}</a>
             </li>
           </Link>
           <Link href="/user/security">
             <li className={router.pathname == "/user/security" ? "active" : ""}>
+              <AiFillSecurityScan />
               <a href="/user/security-setting">{t("Security")}</a>
             </li>
           </Link>
@@ -42,6 +53,7 @@ const ProfileSidebar = () => {
                 router.pathname == "/user/personal-verification" ? "active" : ""
               }
             >
+              <MdOutlineDomainVerification />
               <a href="/user/profile-verification-list">
                 {t("KYC Verification")}
               </a>
@@ -64,6 +76,7 @@ const ProfileSidebar = () => {
                 router.pathname == "/user/change-password" ? "active" : ""
               }
             >
+              <MdPassword />
               <a href="/user/change-password">{t("Change Password")}</a>
             </li>
           </Link>
