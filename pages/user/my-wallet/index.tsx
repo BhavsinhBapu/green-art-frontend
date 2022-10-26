617e527919a65926069222564dbb52b4683f070b
Naim <naimkst@gmail.com>
2022-10-26 12:40:00 +0600

    responsive issue solve

--- pages/user/my-wallet/index.tsx
+++ pages/user/my-wallet/index.tsx
@@ -45,7 +45,6 @@ const MyWallet: NextPage = ({
   const [allData, setAllData] = useState<any>();
   const [tradeList, setTradeList]: any = useState();
   const [coinList, setCoinList]: any = useState([]);
-  const [screenSize, setScreenSize]: any = useState([]);
   const handleActive = (index: any) => {
     console.log(index);
     if (index === tradeList) {
@@ -144,12 +143,7 @@ const MyWallet: NextPage = ({
     setCoinList(coinList);
   };
 
-  console.log(screenSize);
-
   useEffect(() => {
-    let screenSizeGet = screen.width;
-    setScreenSize(screenSizeGet);
-
     coinListApi();
     getWalletLists("/wallet-list?page=1&per_page=15");
     return () => {
@@ -301,10 +295,7 @@ const MyWallet: NextPage = ({
                       </div>
                     </div>
                   </div>
-                  <div
-                    className="table-responsive"
-                    style={{ width: screenSize - 40 }}
-                  >
+                  <div className="table-responsive tableScroll">
                     {processing ? (
                       <Loading />
                     ) : (
