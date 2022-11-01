45681ee217f0b08011cfc3baf117e8ba09185ed2
anirban00537 <anirban00537@gmail.com>
2022-11-01 16:23:13 +0600

    updated
    Former-commit-id: 79131a91dc1053dfba911b0802b0f3314a561570

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -4,73 +4,35 @@ import React from "react";
 
 const FAQ = ({ faqs, type }: any) => {
   const { t } = useTranslation("common");
-  let typeData: any;
-  if (type === "deposit") {
-    typeData = FAQ_TYPE_DEPOSIT;
-  } else if (type === "withdraw") {
-    typeData = FAQ_TYPE_WITHDRAWN;
-  }
   return (
     <div className="m-3">
       <div id="accordion">
         <h4>{t("FAQ")}</h4>
-        {type
-          ? faqs.map(
-              (faq: any) =>
-                faq.faq_type_id === typeData && (
-                  <div className="faq-body">
-                    <div className="faq-head" id={"headingOne" + faq.id}>
-                      <h5 className="mb-0">
-                        <button
-                          className="btn "
-                          data-toggle="collapse"
-                          data-target={"#collapseOne" + faq.id}
-                          aria-expanded="true"
-                          aria-controls="collapseOne"
-                        >
-                          {faq.question}
-                        </button>
-                      </h5>
-                    </div>
-                    <div
-                      id={"collapseOne" + faq.id}
-                      className="collapse "
-                      aria-labelledby={"headingOne" + faq.id}
-                      data-parent="#accordion"
-                    >
-                      <div className="faq-body">{faq.answer}</div>
-                    </div>
-                  </div>
-                )
-            )
-          : faqs.map(
-              (faq: any) =>
-                faq.faq_type_id === FAQ_TYPE_DEPOSIT && (
-                  <div className="faq-body">
-                    <div className="faq-head" id={"headingOne" + faq.id}>
-                      <h5 className="mb-0">
-                        <button
-                          className="btn "
-                          data-toggle="collapse"
-                          data-target={"#collapseOne" + faq.id}
-                          aria-expanded="true"
-                          aria-controls="collapseOne"
-                        >
-                          {faq.question}
-                        </button>
-                      </h5>
-                    </div>
-                    <div
-                      id={"collapseOne" + faq.id}
-                      className="collapse "
-                      aria-labelledby={"headingOne" + faq.id}
-                      data-parent="#accordion"
-                    >
-                      <div className="faq-body">{faq.answer}</div>
-                    </div>
-                  </div>
-                )
-            )}
+        {faqs.map((faq: any) => (
+          <div className="faq-body">
+            <div className="faq-head" id={"headingOne" + faq?.id}>
+              <h5 className="mb-0">
+                <button
+                  className="btn "
+                  data-toggle="collapse"
+                  data-target={"#collapseOne" + faq?.id}
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  {faq?.question}
+                </button>
+              </h5>
+            </div>
+            <div
+              id={"collapseOne" + faq?.id}
+              className="collapse "
+              aria-labelledby={"headingOne" + faq?.id}
+              data-parent="#accordion"
+            >
+              <div className="faq-body">{faq?.answer}</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
