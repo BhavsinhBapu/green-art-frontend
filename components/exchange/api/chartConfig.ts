9df30116eb887ff2dfa9a2679a8f18c5f6f9f368
anirban00537 <anirban00537@gmail.com>
2022-11-04 21:50:33 +0600

    updated

--- components/exchange/api/chartConfig.ts
+++ components/exchange/api/chartConfig.ts
@@ -12,7 +12,7 @@ export const DISABLED_FEATURES = [
   // "header_symbol_search",
   // "header_widget",
   "symbol_info",
-  // "header_compare",
+  "header_compare",
   // "header_chart_type",
   // "display_market_status",
   // "symbol_search_hot_key",
