2b6b8bfe4fc3ccfc91720b566f20ad9aedd7eded
anirban00537 <anirban00537@gmail.com>
2022-11-07 17:41:27 +0600

    work in progress
    Former-commit-id: 5572f76829b51e8cbb700f9a8f71bbdbe6b1657a
    Former-commit-id: d1e083b7d97a8d191e404de35ac7835bf4429411

--- pages/launchpad/apply.tsx
+++ pages/launchpad/apply.tsx
@@ -1,12 +1,24 @@
+import { FORM_INPUT_TEXT } from "helpers/core-constants";
 import React, { useEffect, useState } from "react";
 import { launchpadDynamicFromAction } from "state/actions/launchpad";
 
 const Apply = () => {
-  const [launchpadForm, setLaunchpadForm] = useState();
+  const [launchpadForm, setLaunchpadForm] = useState([]);
   useEffect(() => {
-    launchpadDynamicFromAction();
+    launchpadDynamicFromAction(setLaunchpadForm);
   }, []);
-  return <div>Apply</div>;
+  return (
+    <div className="container">
+      {JSON.stringify(launchpadForm)}
+      <input type="number" className="form-control" id="amount-one" />
+      {launchpadForm?.map(
+        (item: any) =>
+          item.type === FORM_INPUT_TEXT && (
+            <input type="number" className="form-control" id="amount-one" />
+          )
+      )}
+    </div>
+  );
 };
 
 export default Apply;
