bfc05909d68053523bcf67b37e1161149f4ed6f0
anirban00537 <anirban00537@gmail.com>
2022-11-07 16:06:39 +0600

    build issue fixed
    Former-commit-id: e575556ea25d1109340849a8b259a5381c235d40

--- pages/launchpad/apply.tsx
+++ pages/launchpad/apply.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { launchpadDynamicFromAction } from "state/actions/launchpad";
 
-const apply = () => {
+const Apply = () => {
   const [launchpadForm, setLaunchpadForm] = useState();
   useEffect(() => {
     launchpadDynamicFromAction();
   }, []);
-  return <div>apply</div>;
+  return <div>Apply</div>;
 };
 
-export default apply;
+export default Apply;
