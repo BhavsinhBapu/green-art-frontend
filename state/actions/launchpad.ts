c0fe470f3156edbcb2083d3b812308bc28d5a397
Naim <naimkst@gmail.com>
2022-11-08 12:15:55 +0600

    ICO Listing done

--- state/actions/launchpad.ts
+++ state/actions/launchpad.ts
@@ -6,9 +6,17 @@ import {
   launchpadDynamicFromSubmit,
 } from "service/launchpad";
 
-export const getLaunchpadListAction = async (setLaunchpadList: any) => {
+export const getLaunchpadListAction = async (
+  setLaunchpadList: any,
+  setLaunchpadFeatureItem: any
+) => {
+  let featuredItem: any = [];
   const response = await getLaunchpadList();
+  response?.data?.map((item: any, index: number) => {
+    item.is_featured == 1 && featuredItem.push(item);
+  });
   setLaunchpadList(response);
+  setLaunchpadFeatureItem(featuredItem);
 };
 export const getLaunchpadListDetailsAction = async (
   setLaunchpadListDetails: any,
