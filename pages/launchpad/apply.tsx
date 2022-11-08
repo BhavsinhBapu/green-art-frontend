5164fef49dd8527ca40ec4e9d7598464aa32abea
Naim <naimkst@gmail.com>
2022-11-08 17:29:46 +0600

    Form design

--- pages/launchpad/apply.tsx
+++ pages/launchpad/apply.tsx
@@ -10,13 +10,62 @@ const Apply = () => {
   return (
     <div className="container">
       {JSON.stringify(launchpadForm)}
-      <input type="number" className="form-control" id="amount-one" />
-      {launchpadForm?.map(
+      <div className="appy-form">
+        <div className="form-div">
+          <label htmlFor="">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority
+          </label>
+          <input type="number" className="form-control apply-input" id="" />
+        </div>
+
+        <div className="form-div">
+          <label htmlFor="">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority
+          </label>
+          <select className="form-control apply-select-field" name="" id="">
+            <option value="">A</option>
+            <option value="">B</option>
+            <option value="">C</option>
+            <option value="">D</option>
+          </select>
+        </div>
+
+        <div className="form-div">
+          <label htmlFor="">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority
+          </label>
+          <input
+            className="form-control apply-checkbox-input"
+            type="checkbox"
+          />
+        </div>
+
+        <div className="form-div">
+          <label htmlFor="">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority
+          </label>
+          <input className="form-control apply-radio-input" type="radio" />
+        </div>
+
+        <div className="form-div">
+          <label htmlFor="">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority
+          </label>
+          <input className="apply-file-input" type="file" />
+        </div>
+      </div>
+
+      {/* {launchpadForm?.map(
         (item: any) =>
           item.type === FORM_INPUT_TEXT && (
             <input type="number" className="form-control" id="amount-one" />
           )
-      )}
+      )} */}
     </div>
   );
 };
