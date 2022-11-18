c6a3ae6bcb7d1b59f1a2f2b6ad84bcb604173f5c
Naim <naimkst@gmail.com>
2022-11-18 16:37:23 +0600

    ico create form done
    Former-commit-id: 5a0f0e6d94f694407fa4b7a55350d52da65f6c8b
    Former-commit-id: d3dd86dc587f0dfbbf8e1ceaaa1c067dbf7f206c

--- pages/ico/create-token/[id].tsx
+++ pages/ico/create-token/[id].tsx
@@ -1,7 +1,113 @@
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
 
 const TokenCreate = () => {
-  return <div>TokenCreate</div>;
+  const { t } = useTranslation("common");
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="ico-tokenCreate">
+          <div className="col-12">
+            <h2>Add New ICO Token</h2>
+          </div>
+          <div className="ico-create-form col-12">
+            <form action="" className="row">
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t(" ICO Submit Form ID")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t(" Base Coin")}
+                </label>
+                <select name="" className="ico-input-box">
+                  <option value="">{"Select Base Coin"}</option>
+                  <option value="BTC">{"BTC"}</option>
+                  <option value="ETC">{"ETC"}</option>
+                  <option value="BNB">{"BNB"}</option>
+                </select>
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Token Name")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Network")}
+                </label>
+                <select name="" className="ico-input-box">
+                  <option value="">{"Select Base Coin"}</option>
+                  <option value="4">{"ERC20 Token Api"}</option>
+                  <option value="5">{"BEP20 Token Api"}</option>
+                </select>
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Wallet Address")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Contract Address")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Wallet Private Key")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Chain Id")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Chain Link")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Decimal")}
+                </label>
+                <select name="" className="ico-input-box">
+                  <option value="">{"Select Your Decimal"}</option>
+                  <option value="6">{"6"}</option>
+                  <option value="12">{"12"}</option>
+                  <option value="15">{"15"}</option>
+                  <option value="18">{"18"}</option>
+                  <option value="21">{"21"}</option>
+                  <option value="24">{"24"}</option>
+                  <option value="27">{"27"}</option>
+                  <option value="30">{"30"}</option>
+                </select>
+              </div>
+              <div className="col-md-6 form-input-div">
+                <label className="ico-label-box" htmlFor="">
+                  {t("Gas Limit")}
+                </label>
+                <input type="text" className="ico-input-box" />
+              </div>
+
+              <div className="col-md-12 form-input-div">
+                <button className="primary-btn">Apply To Launch</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TokenCreate;
