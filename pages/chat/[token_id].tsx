491b2d95583cf3596a0be80f6166c6c04b5e2a86
anirban00537 <anirban00537@gmail.com>
2022-12-02 15:54:36 +0600

    updated
    Former-commit-id: aa234c06295014051da3aa957be92e15fcfbb73f
    Former-commit-id: c2b0e91a272988f447c1d096bc69367ce0795974

--- pages/chat/[token_id].tsx
+++ pages/chat/[token_id].tsx
@@ -2,14 +2,67 @@ import Footer from "components/common/footer";
 import { SSRAuthCheck } from "middlewares/ssr-authentication-check";
 import { GetServerSideProps } from "next";
 import useTranslation from "next-translate/useTranslation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { customPage, landingPage } from "service/landing-page";
 import { TiDelete } from "react-icons/ti";
+import { useRouter } from "next/router";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+import {
+  ChatHistoryByTokenIdAction,
+  SendChantByTokenAction,
+} from "state/actions/launchpad";
+import { useSelector } from "react-redux";
+import { RootState } from "state/store";
+import { date } from "yup";
+let socketCall = 0;
 
 export const Chat = ({ customPageData, socialData, copyright_text }: any) => {
   const { t } = useTranslation("common");
   const [file, setFile] = useState<any>();
-
+  const [sendFile, setSendFile] = useState();
+  const [message, setMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState<any>([]);
+  const { user } = useSelector((state: RootState) => state.user);
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router.query.token_id, "router.query.token_id");
+    router.query.token_id &&
+      ChatHistoryByTokenIdAction(setChatHistory, router.query.token_id);
+  }, [router.query.token_id]);
+  async function listenMessages() {
+    //@ts-ignore
+    window.Pusher = Pusher;
+    //@ts-ignore
+    window.Echo = new Echo({
+      broadcaster: "pusher",
+      key: "test",
+      wsHost: process.env.NEXT_PUBLIC_HOST_SOCKET,
+      wsPort: 6006,
+      wssPort: 443,
+      forceTLS: false,
+      cluster: "mt1",
+      disableStats: true,
+      enabledTransports: ["ws", "wss"],
+    });
+    //@ts-ignore
+    // dashboard-base_coin_id-trade_coin_id
+    window.Echo.channel(
+      `New-Message-${localStorage.getItem("user_id")}-${router.query.token_id}`
+    ).listen(".Conversation", (e: any) => {
+      let chat = chatHistory;
+      chat.push(e.data);
+      console.log(e.data, "chatchatchat");
+      setChatHistory(chat);
+    });
+  }
+  useEffect(() => {}, [chatHistory]);
+  useEffect(() => {
+    if (socketCall === 0) {
+      listenMessages();
+    }
+    socketCall = 1;
+  });
   return (
     <>
       <div className="page-wrap rightMargin">
@@ -38,61 +91,53 @@ export const Chat = ({ customPageData, socialData, copyright_text }: any) => {
                           alt=""
                         />
                         <div className="chart-header-title">
-                          <p className="chat-name">Rolex</p>
-                          <p className="chat-status ofline online">Offline</p>
-                          <p className="chat-last-seen">
+                          <p className="chat-name">Admin</p>
+                          {/* <p className="chat-status ofline online">Offline</p> */}
+                          {/* <p className="chat-last-seen">
                             Last seen 2 months ago
-                          </p>
+                          </p> */}
                         </div>
                       </div>
                       <div className="chat-body">
-                        <div className="chat-left">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="chat-left">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="chat-left">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
+                        {chatHistory?.map((chat: any) => (
+                          <>
+                            <div
+                              className={
+                                chat.sender_id === user.id
+                                  ? "chat-right"
+                                  : "chat-left"
+                              }
+                            >
+                              <>
+                                <img
+                                  className="chat-list-avatar"
+                                  height={35}
+                                  width={35}
+                                  src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
+                                  alt=""
+                                />
+                                <div className="chart-header-title">
+                                  <p className="chat-details">
+                                    {chat?.message}
+                                  </p>
+                                  <p className="chat-last-seen">
+                                    {chat?.created_at}
+                                  </p>
+                                </div>
+                              </>
+                            </div>
+                            {chat?.file_path && (
+                              <img
+                                className="image-chatBox"
+                                height={65}
+                                width={105}
+                                src={chat.file_path}
+                                alt=""
+                              />
+                            )}
+                          </>
+                        ))}
+                        {/* 
                         <div className="chat-right">
                           <img
                             className="chat-list-avatar"
@@ -107,55 +152,7 @@ export const Chat = ({ customPageData, socialData, copyright_text }: any) => {
                               Last seen 2 months ago
                             </p>
                           </div>
-                        </div>
-
-                        <div className="chat-right">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="chat-right">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="chat-right">
-                          <img
-                            className="chat-list-avatar"
-                            height={35}
-                            width={35}
-                            src="https://tradexpro-app.cdibrandstudio.com/assets/img/avater.png"
-                            alt=""
-                          />
-                          <div className="chart-header-title">
-                            <p className="chat-details">Rolex</p>
-                            <p className="chat-last-seen">
-                              Last seen 2 months ago
-                            </p>
-                          </div>
-                        </div>
+                        </div> */}
                       </div>
                       {file && (
                         <div className="chat-upload-image">
@@ -178,8 +175,9 @@ export const Chat = ({ customPageData, socialData, copyright_text }: any) => {
                           <label className="upload-file-btn press-enter">
                             <span className="fa fa-paperclip press-enter"></span>
                             <input
-                              onChange={(e) => {
+                              onChange={(e: any) => {
                                 setFile(URL.createObjectURL(e.target.files[0]));
+                                setSendFile(e.target.files[0]);
                               }}
                               id="msgFile"
                               type="file"
@@ -194,11 +192,30 @@ export const Chat = ({ customPageData, socialData, copyright_text }: any) => {
                             type="text"
                             className="chatInput"
                             placeholder="Type here.."
+                            value={message}
+                            onChange={(e: any) => {
+                              setMessage(e.target.value);
+                            }}
                           />
                         </div>
 
                         <div className="submit-button">
-                          <button className="chat-button">Send</button>
+                          <button
+                            className="chat-button"
+                            disabled={!message}
+                            onClick={() => {
+                              SendChantByTokenAction(
+                                setChatHistory,
+                                router.query.token_id,
+                                sendFile,
+                                message,
+                                chatHistory,
+                                setMessage
+                              );
+                            }}
+                          >
+                            {t("Send")}
+                          </button>
                         </div>
                       </div>
                     </div>
