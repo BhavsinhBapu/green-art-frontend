cee75a4dfc0be93178171fe33a501bd1468655b5
anirban00537 <anirban00537@gmail.com>
2022-12-22 18:08:10 +0600

    updated
    Former-commit-id: 420ef827ceae49bae5ef9fcc6a4b62d53ab6cfbe
    Former-commit-id: de5704f614cef5a4ed95afad1f872ab01b8f4245

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -1,24 +1,70 @@
-export const checkDarkMode = () => {
+export const checkDarkMode = (settings: any) => {
   const theme = localStorage.getItem("theme");
   if (theme === "light") {
     localStorage.setItem("theme", "light");
     document.documentElement.setAttribute("data-theme", "light");
+    settings.theme_color.map((themeColors: any) => {
+      if (!themeColors.value) {
+        return;
+      }
+      document.documentElement.style.setProperty(
+        themeColors.name,
+        themeColors.value
+      );
+    });
+  } else {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+    settings.dark_theme_color.map((themeColors: any) => {
+      if (!themeColors.value) {
+        return;
+      }
+      document.documentElement.style.setProperty(
+        themeColors.name,
+        themeColors.value
+      );
+    });
+  }
+};
+export const checkDarkModeWithoutSettings = () => {
+  const theme = localStorage.getItem("theme");
+  if (theme === "light") {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "dark");
   } else {
     localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
   }
 };
-
-export const darkModeToggle = () => {
+export const darkModeToggle = (settings: any) => {
   const theme = localStorage.getItem("theme");
   if (theme === "dark") {
+    settings.theme_color.map((theme_color: any) => {
+      if (!theme_color.value) {
+        return;
+      }
+      document.documentElement.style.setProperty(
+        theme_color.name,
+        theme_color.value
+      );
+    });
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
+    settings.dark_theme_color.map((theme_color: any) => {
+      if (!theme_color.value) {
+        return;
+      }
+      document.documentElement.style.setProperty(
+        theme_color.name,
+        theme_color.value
+      );
+    });
     localStorage.setItem("theme", "dark");
   }
 };
+
 export const darkModeToggleDashboard = () => {
   const theme = localStorage.getItem("theme");
   console.log(theme, "</button>");
