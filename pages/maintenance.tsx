a4a3009039f96fe94a1f1efb37fee11b362c5333
anirban00537 <anirban00537@gmail.com>
2023-01-17 12:20:04 +0600

    updated maintance page
    Former-commit-id: 658856bc4fea5327e924f62a08a45972fa4864c8

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GetServerSideProps } from "next";
 import { commomSettings } from "service/landing-page";
 import useTranslation from "next-translate/useTranslation";
+import { redirect } from "next/dist/server/api-utils";
 
 const Maintenance = ({ data }: any) => {
   const { t } = useTranslation("common");
@@ -41,6 +42,15 @@ const Maintenance = ({ data }: any) => {
 
 export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
   const { data } = await commomSettings();
+  console.log(data.maintenance_mode_status, "data");
+  if (parseInt(data?.maintenance_mode_status) === 0) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
   return {
     props: { data },
   };
