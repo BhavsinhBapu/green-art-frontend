b3e228f0a71c839f7a0e7d3c08eca8a8160dc894
anirban00537 <anirban00537@gmail.com>
2023-01-19 19:26:12 +0600

    basic blog setup

---
+++ components/Blog/TabSection.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TabSection = () => {
+  return (
+    <div className="d-flex mt-5">
+      <div className="itemCatagory itemCatagoryactive">
+        <p>One</p>
+      </div>
+      <div className="itemCatagory">
+        <p>One</p>
+      </div>
+      <div className="itemCatagory">
+        <p>One</p>
+      </div>
+      <div className="itemCatagory">
+        <p>One</p>
+      </div>
+      <div className="itemCatagory">
+        <p>One</p>
+      </div>
+      <div className="itemCatagory">
+        <p>One</p>
+      </div>
+    </div>
+  );
+};
+
+export default TabSection;
