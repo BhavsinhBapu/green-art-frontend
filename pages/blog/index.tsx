06853afa01c0020274c4d94cf3c1b803ab38a125
anirban00537 <anirban00537@gmail.com>
2023-01-20 16:35:03 +0600

    blog api added
    Former-commit-id: 0b0afa22e155f53fcbe87dfc453ad224eb1f9706

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,16 +4,25 @@ import TabSection from "components/Blog/TabSection";
 import Footer from "components/common/footer";
 import { SSRAuthCheck } from "middlewares/ssr-authentication-check";
 import { GetServerSideProps } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { customPage, landingPage } from "service/landing-page";
+import { BlogHomePageAction } from "state/actions/blog";
 
-const index = ({ customPageData, socialData, copyright_text }: any) => {
+const index = ({
+  customPageData,
+  socialData,
+  copyright_text,
+  featuredBlogs,
+  recentBlogs,
+}: any) => {
+  console.log(featuredBlogs, "featuredBlogs");
+  console.log(recentBlogs, "recentBlogs");
   return (
     <>
       <div className="container">
         <SliderCover />
         <TabSection />
-        <CardSection />
+        <CardSection blogs={recentBlogs} />
       </div>
       <Footer
         customPageData={customPageData}
@@ -28,11 +37,15 @@ export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
 
   const { data } = await landingPage();
   const { data: customPageData } = await customPage();
+
+  const { FeaturedBlogs, RecentBlogs } = await BlogHomePageAction();
   return {
     props: {
       socialData: data.media_list,
       copyright_text: data?.copyright_text,
       customPageData: customPageData.data,
+      featuredBlogs: FeaturedBlogs?.data,
+      recentBlogs: RecentBlogs?.data,
     },
   };
 };
