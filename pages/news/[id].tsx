2e1eb7e2b4040304fc312401fee592e9fd789158
anirban00537 <anirban00537@gmail.com>
2023-01-25 12:09:15 +0600

    developed changes

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -1,39 +1,12 @@
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { BiChevronLeft } from "react-icons/bi";
-import {
-  EmailShareButton,
-  FacebookIcon,
-  FacebookShareButton,
-  HatenaShareButton,
-  InstapaperIcon,
-  InstapaperShareButton,
-  LineShareButton,
-  LinkedinShareButton,
-  LivejournalShareButton,
-  MailruShareButton,
-  OKShareButton,
-  PinterestShareButton,
-  PocketShareButton,
-  RedditIcon,
-  RedditShareButton,
-  TelegramShareButton,
-  TumblrShareButton,
-  TwitterIcon,
-  TwitterShareButton,
-  ViberShareButton,
-  VKShareButton,
-  WhatsappIcon,
-  WhatsappShareButton,
-  WorkplaceShareButton,
-} from "react-share";
-import { FaFacebookF } from "react-icons/fa";
-import { AiOutlineReddit } from "react-icons/ai";
-import { FaTelegramPlane } from "react-icons/fa";
+
 import { SSRAuthCheck } from "middlewares/ssr-authentication-check";
 import { getNewsDetails } from "service/news";
 import { GetServerSideProps } from "next";
 import { formateData } from "common";
+import SocialShare from "components/common/SocialShare";
 
 const NewsDetails = ({ newsDetails }: any) => {
   const { t } = useTranslation("common");
@@ -74,73 +47,13 @@ const NewsDetails = ({ newsDetails }: any) => {
               </div>
             </div>
           </div>
-          <div className="col-md-4 ">
-            <h4 className="mt-4 ">{t("Share")}</h4>
-            <div className="my-3 newsShare d-flex align-items-center">
-              <a href="">
-                 
-                <FacebookShareButton
-                  url={
-                    process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
-                    "news/" +
-                    newsDetails?.details?.post_id
-                  }
-                  quote={"Share on facebook"}
-                  hashtag="#muo"
-                >
-                          
-                  <FacebookIcon size={32} round />
-                        
-                </FacebookShareButton>
-              </a>
-              <a href="">
-                <TwitterShareButton
-                  url={
-                    process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
-                    "news/" +
-                    newsDetails?.details?.post_id
-                  }
-                >
-                          
-                  <TwitterIcon size={32} round />
-                        
-                </TwitterShareButton>
-              </a>
-              <a href="">
-                <RedditShareButton
-                  url={
-                    process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
-                    "news/" +
-                    newsDetails?.details?.post_id
-                  }
-                >
-                  <RedditIcon size={32} round />
-                </RedditShareButton>
-              </a>
-              <a href="">
-                <InstapaperShareButton
-                  url={
-                    process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
-                    "news/" +
-                    newsDetails?.details?.post_id
-                  }
-                >
-                  <InstapaperIcon size={32} round />
-                </InstapaperShareButton>
-              </a>
-              <a href="">
-                <WhatsappShareButton
-                  url={
-                    process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
-                    "news/" +
-                    newsDetails?.details?.post_id
-                  }
-                >
-                  <WhatsappIcon size={32} round />
-                </WhatsappShareButton>
-              </a>
-            </div>
-          </div>
+          <SocialShare
+            url={
+              process.env.NEXT_PUBLIC_HOSTED_CLIENT_URL +
+              "news/" +
+              newsDetails?.details?.post_id
+            }
+          />
         </div>
       </div>
     </>
