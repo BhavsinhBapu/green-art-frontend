f6e7d82c04a3958d456cd4568f61e1c61a13e17b
sajal <sajal2020roy@gmail.com>
2023-02-01 16:07:55 +0600

    card-ui

--- pages/knowledgebase/[article_id].tsx
+++ pages/knowledgebase/[article_id].tsx
@@ -1,9 +1,9 @@
 import { formateData } from "common";
-import { ArticalCard } from "components/Knowledgebase/article-card";
 import { GetServerSideProps } from "next";
 import { articleDetails } from "service/knowledgebase";
 import { customPage, landingPage } from "service/landing-page";
 import { getNewsDetails } from "service/news";
+import Link from "next/link";
 
 const KnowledgebaseArticleDetails = ({ articleDetails }: any) => {
   console.log(articleDetails, "articleDetails");
@@ -11,7 +11,7 @@ const KnowledgebaseArticleDetails = ({ articleDetails }: any) => {
     <>
       <div className="container mb-5">
         <div className="row">
-          <div className="col-md-8 mt-4">
+          <div className="col-md-6 col-lg-8 mt-4">
             <div className="main_img">
               {articleDetails.article_details?.feature_image && (
                 <img
@@ -37,34 +37,30 @@ const KnowledgebaseArticleDetails = ({ articleDetails }: any) => {
           <div className="col-md-6 col-lg-4 mt-5 mt-lg-0 pt-4 ">
             <div className="row">
               {articleDetails.related_article_list.map((article: any) => (
-                <div className="col-12 mt-4 mb-4">
+                <div className="col-12 mt-4 mb-5">
                   <div className="sub_title px-4 pt-4 pb-1 h-100">
                     <h4 className="fw_600 pt-3 mb-0">
                       <span className="mr-2 h5">
                         <i className="fa fa-address-card"></i>
                       </span>
-                      What is Lorem Ipsum?
+                      {article?.title}
                     </h4>
-                    <small className="article-date">12-02-2000</small>
-                    <p className="p_color pt-3">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book.
-                    </p>
+                    <small className="article-date">
+                      {formateData(article?.created_at)}
+                    </small>
+                    <p className="p_color pt-3">{article?.description}</p>
                   </div>
-                  <div className="details-button">
-                    <a href="#">
-                      view more
-                      <i
-                        className="ml-2 fa fa-long-arrow-right"
-                        aria-hidden="true"></i>
-                    </a>
-                  </div>
+                  <Link href={"/knowledgebase/" + article.unique_code}>
+                    <div className="details-button">
+                      <a href="#">
+                        view more
+                        <i
+                          className="ml-2 fa fa-long-arrow-right"
+                          aria-hidden="true"></i>
+                      </a>
+                    </div>
+                  </Link>
                 </div>
-
-                // <ArticalCard article={article} />
               ))}
             </div>
           </div>
