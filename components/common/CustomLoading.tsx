9d7745ed2566cb6c6cf795d164c14ff9d5da28ab
sajal <sajal2020roy@gmail.com>
2023-02-01 16:28:19 +0600

    lodding-footer-ui

--- components/common/CustomLoading.tsx
+++ components/common/CustomLoading.tsx
@@ -1,9 +1,11 @@
 export const CustomLoading = () => {
   return (
-    <div className="custom-loading-conatiner">
-      <div className="d-flex justify-content-center">
-        <div className="spinner-border loadingSpinner" role="status">
-          <span className="sr-only">Loading...</span>
+    <div className="container">
+      <div className="custom-loading-conatiner loading_box">
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border loadingSpinner" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
       </div>
     </div>
