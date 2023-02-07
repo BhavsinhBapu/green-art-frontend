71eccc204c259af683bbb770d28f1c6d0308a2cf
sajal <sajal2020roy@gmail.com>
2023-02-07 20:22:48 +0600

    p2p-ui

--- components/P2P/TopBar.tsx
+++ components/P2P/TopBar.tsx
@@ -1,22 +1,46 @@
+import { RiPagesLine } from "react-icons/ri";
+import { HiUsers } from "react-icons/hi";
+
 export const P2pTopBar = () => {
   return (
-    <div className="bg-dark py-5">
+    <div className="bg-dark py-3">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="">
-              <ul>
-                <li>
-                  <a href="">Orders</a>
-                </li>
-                <li>
-                  <a href="">P2P User Center</a>
-                </li>
-                <li>
-                  <a href="">More</a>
-                </li>
-              </ul>
-            </div>
+            <ul className="d-flex float-right topBarList">
+              <li>
+                <a href="">
+                  <RiPagesLine />
+                  Orders
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <HiUsers />
+                  P2P User Center
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="dropdown-toggle"
+                  data-toggle="dropdown"
+                  aria-expanded="false">
+                  More
+                </a>
+                <div className="dropdown-menu mt-3 bg-dark">
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- pages/p2p/index.tsx
+++ pages/p2p/index.tsx
@@ -3,23 +3,24 @@ import { P2pTopBar } from "components/P2P/TopBar";
 const P2P = () => {
   return (
     <>
-      <div className="p2p_bg">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 text-center text-white">
-              <h2 className="text-white">
-                Binance - Peer-to-Peer Ecosystem With 300+Payment Methods
-              </h2>
-              <p>
-                Binance is the largest centralized exchange globally. However,
-                Binance is also a major player <br /> in the P2P trading space
-              </p>
+      <div>
+        <div className="p2p_bg">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 text-center text-white">
+                <h2 className="text-white">
+                  Binance - Peer-to-Peer Ecosystem With 300+Payment Methods
+                </h2>
+                <p>
+                  Binance is the largest centralized exchange globally. However,
+                  Binance is also a major player <br /> in the P2P trading space
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <P2pTopBar />
       </div>
-
-      <P2pTopBar />
     </>
   );
 };
