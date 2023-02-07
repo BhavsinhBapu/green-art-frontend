7adf92c4e77f7f45f08127e74b9752119caf37e1
sajal <sajal2020roy@gmail.com>
2023-02-07 15:38:05 +0600

    p2p-ui

---
+++ components/P2P/TopBar.tsx
@@ -0,0 +1,25 @@
+export const P2pTopBar = () => {
+  return (
+    <div className="bg-dark py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="">
+              <ul>
+                <li>
+                  <a href="">Orders</a>
+                </li>
+                <li>
+                  <a href="">P2P User Center</a>
+                </li>
+                <li>
+                  <a href="">More</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
