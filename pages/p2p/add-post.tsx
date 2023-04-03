884665b089c210e7a7c5ea99234069562f05373c
anirban00537 <anirban00537@gmail.com>
2023-04-03 11:41:49 +0600

    work in progress

--- pages/p2p/add-post.tsx
+++ pages/p2p/add-post.tsx
@@ -1,59 +1,71 @@
 import Footer from "components/common/footer";
+import SectionLoading from "components/common/SectionLoading";
 import { AddPostSteep } from "components/P2P/AddPost/AddPostSteep";
 import { AddPostOne } from "components/P2P/AddPost/AddPostSteep_one";
 import { AddPostThree } from "components/P2P/AddPost/AddPostSteep_three";
 import { AddPostTwo } from "components/P2P/AddPost/AddPostSteep_two";
 import { P2pTopBar } from "components/P2P/P2pHome/TopBar";
 import { useState } from "react";
+import { useAddPostInitial } from "state/actions/p2p";
 
 const AddPost = () => {
+  const { data, loading, error } = useAddPostInitial();
   const [addTabButton, setAddTabButton] = useState(true);
   const [addSteep, setAddSteep] = useState("steepOne");
-
   return (
     <>
-      <div className="mb-5">
-        <P2pTopBar />
-        <div className="adPost_bg py-3">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <h3>Post Normal Ad</h3>
+      {loading ? (
+        <SectionLoading />
+      ) : (
+        <div className="mb-5">
+          <P2pTopBar />
+
+          <div className="adPost_bg py-3">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <h3>Post Normal Ad</h3>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <AddPostSteep />
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <button
-                className={`addTabButton ${addTabButton && "buySellBoxActive"}`}
-                onClick={() => setAddTabButton(true)}>
-                I Want to Buy
-              </button>
+          <AddPostSteep />
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <button
+                  className={`addTabButton ${
+                    addTabButton && "buySellBoxActive"
+                  }`}
+                  onClick={() => setAddTabButton(true)}
+                >
+                  I Want to Buy
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  className={`addTabButton ${
+                    !addTabButton && "buySellBoxActive"
+                  }`}
+                  onClick={() => setAddTabButton(false)}
+                >
+                  I Want to Sell
+                </button>
+              </div>
+              {addSteep === "steepOne" && (
+                <AddPostOne setAddSteep={setAddSteep} />
+              )}
+              {addSteep === "steepTwo" && (
+                <AddPostTwo setAddSteep={setAddSteep} />
+              )}
+              {addSteep === "steepThree" && (
+                <AddPostThree setAddSteep={setAddSteep} />
+              )}
             </div>
-            <div className="col-6">
-              <button
-                className={`addTabButton ${
-                  !addTabButton && "buySellBoxActive"
-                }`}
-                onClick={() => setAddTabButton(false)}>
-                I Want to Sell
-              </button>
-            </div>
-            {addSteep === "steepOne" && (
-              <AddPostOne setAddSteep={setAddSteep} />
-            )}
-            {addSteep === "steepTwo" && (
-              <AddPostTwo setAddSteep={setAddSteep} />
-            )}
-            {addSteep === "steepThree" && (
-              <AddPostThree setAddSteep={setAddSteep} />
-            )}
           </div>
         </div>
-      </div>
+      )}
+
       <Footer />
     </>
   );
--- state/actions/p2p.ts
+++ state/actions/p2p.ts
@@ -1,5 +1,7 @@
+import { useApi } from "helpers/hooks";
 import { getAdsCreateSettings } from "service/p2p";
 
-export const useAddPostInitial = async () => {
-    
+export const useAddPostInitial = () => {
+  const { data, loading, error,refreshData } = useApi(getAdsCreateSettings);
+  return { data, loading, error, refreshData };
 };
