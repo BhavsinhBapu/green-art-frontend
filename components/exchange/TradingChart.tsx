1af2b24e3fc354e712854ef2be9bc8cbb8726b59
anirban00537 <anirban00537@gmail.com>
2023-05-22 16:42:38 +0600

    working functional code

--- components/exchange/TradingChart.tsx
+++ components/exchange/TradingChart.tsx
@@ -1,8 +1,7 @@
-import * as React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./index.module.css";
 import { widget } from "../../public/static/charting_library";
 import Datafeed from "components/exchange/api";
-import { connect } from "react-redux";
 
 import {
   DISABLED_FEATURES,
@@ -19,258 +18,102 @@ function getLanguageFromURL() {
     : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-type MyProps = {
+type Props = {
   coinpair: any;
-  dispatch: any;
 };
 
-const mapStateToProps = (state: any) => {
-  return {
-    reduxData: state,
-  };
-};
+export const TVChartContainer: React.FC<Props> = ({ coinpair }) => {
+  const ref = useRef(null);
+  let tvWidget = null;
 
-class TVChartContainer extends React.Component<MyProps> {
-  static defaultProps = {
-    symbol: ``,
-    interval: "15",
-    containerId: "tv_chart_container",
-    libraryPath: "/static/charting_library/",
-    chartsStorageUrl: "https://saveload.tradingview.com",
-    chartsStorageApiVersion: "1.1",
-    clientId: "tradingview.com",
-    userId: "public_user_id",
-    fullscreen: false,
-    autosize: true,
-    studiesOverrides: {},
-    overrides: {
-      "mainSeriesProperties.candleStyle.upColor": "#ffffff",
-      "mainSeriesProperties.candleStyle.downColor": "#000000",
-      "mainSeriesProperties.candleStyle.drawBorder": true,
-      "mainSeriesProperties.candleStyle.borderUpColor": "#00ff00",
-      "mainSeriesProperties.candleStyle.borderDownColor": "#ff0000",
-    },
-  };
-
-  tvWidget = null;
-  //@ts-ignore
-  chartInit = (config) => {
-    const tvWidget = new widget(config);
-    if (document.documentElement.classList.contains("theme-dark")) {
-      document.documentElement.classList.remove("theme-dark");
-    }
-    //@ts-ignore
-    this.tvWidget = tvWidget;
-    //@ts-ignore
-    this.tvWidget.onChartReady(() => {
-      //@ts-ignore
-      this.tvWidget.applyOverrides({
-        "paneProperties.background": theme === "dark" ? "#151515" : "#fff",
-        "paneProperties.backgroundType": "solid",
-        //up and down color change32d777
-        "mainSeriesProperties.candleStyle.upColor": "#32d777",
-        "mainSeriesProperties.candleStyle.downColor": "#dc3545",
+  useEffect(() => {
+    const widgetOptions = {
+      height: 480,
+      width: 1400,
+      symbol: `${coinpair?.replace("_", "/")}`,
+      style: 1,
+      theme: theme === "dark" ? "dark" : "light",
+      datafeed: Datafeed,
+      interval: "15",
+      container: ref.current,
+      library_path: "/static/charting_library/",
+      locale: getLanguageFromURL() || "en",
+      charts_storage_url: "https://saveload.tradingview.com",
+      charts_storage_api_version: "1.1",
+      client_id: "tradingview.com",
+      user_id: "public_user_id",
+      fullscreen: false,
+      autosize: true,
+      studies_overrides: {},
+      overrides: {
+        "mainSeriesProperties.candleStyle.upColor": "#ffffff",
+        "mainSeriesProperties.candleStyle.downColor": "#000000",
         "mainSeriesProperties.candleStyle.drawBorder": true,
-        "mainSeriesProperties.candleStyle.borderUpColor": "#32d777",
-        "mainSeriesProperties.candleStyle.borderDownColor": "#dc3545",
-        "mainSeriesProperties.candleStyle.wickUpColor": "#32d777",
-        "mainSeriesProperties.candleStyle.wickDownColor": "#dc3545",
-        "mainSeriesProperties.hollowCandleStyle.upColor": "#32d777",
-        "mainSeriesProperties.hollowCandleStyle.downColor": "#dc3545",
-        "mainSeriesProperties.hollowCandleStyle.drawWick": true,
-        "mainSeriesProperties.hollowCandleStyle.drawBorder": true,
-        "mainSeriesProperties.hollowCandleStyle.borderUpColor": "#32d777",
-        "mainSeriesProperties.hollowCandleStyle.borderDownColor": "#dc3545",
-        "mainSeriesProperties.hollowCandleStyle.wickUpColor": "#32d777",
-        "mainSeriesProperties.hollowCandleStyle.wickDownColor": "#dc3545",
-        "mainSeriesProperties.haStyle.upColor": "#32d777",
-        "mainSeriesProperties.haStyle.downColor": "#dc3545",
-        "mainSeriesProperties.haStyle.drawWick": true,
-        "mainSeriesProperties.haStyle.drawBorder": true,
-        "mainSeriesProperties.haStyle.borderUpColor": "#32d777",
-        "mainSeriesProperties.haStyle.borderDownColor": "#dc3545",
-        "mainSeriesProperties.haStyle.wickUpColor": "#32d777",
-        "mainSeriesProperties.haStyle.wickDownColor": "#dc3545",
-        "mainSeriesProperties.barStyle.upColor": "#32d777",
-        "mainSeriesProperties.barStyle.downColor": "#dc3545",
-        "mainSeriesProperties.barStyle.barColorsOnPrevClose": false,
-        "mainSeriesProperties.barStyle.dontDrawOpen": false,
-        "mainSeriesProperties.lineStyle.color": "#dc3545",
+        "mainSeriesProperties.candleStyle.borderUpColor": "#00ff00",
+        "mainSeriesProperties.candleStyle.borderDownColor": "#ff0000",
+      },
+      enabled_features: ENABLED_FEATURES,
+      disabled_features: DISABLED_FEATURES,
+      custom_css_url: "css/style.css",
+      time_frames: TIME_FRAMES,
+      toolbar: false,
+    };
+
+    const chartInit = (config) => {
+      const widgetInstance = new widget(config);
+      tvWidget = widgetInstance;
+      tvWidget.onChartReady(() => {
+        tvWidget.applyOverrides({
+          "paneProperties.background": theme === "dark" ? "#151515" : "#fff",
+          "paneProperties.backgroundType": "solid",
+          "mainSeriesProperties.candleStyle.upColor": "#32d777",
+          "mainSeriesProperties.candleStyle.downColor": "#dc3545",
+          "mainSeriesProperties.candleStyle.drawBorder": true,
+          "mainSeriesProperties.candleStyle.borderUpColor": "#32d777",
+          "mainSeriesProperties.candleStyle.borderDownColor": "#dc3545",
+          "mainSeriesProperties.candleStyle.wickUpColor": "#32d777",
+          "mainSeriesProperties.candleStyle.wickDownColor": "#dc3545",
+          "mainSeriesProperties.hollowCandleStyle.upColor": "#32d777",
+          "mainSeriesProperties.hollowCandleStyle.downColor": "#dc3545",
+          "mainSeriesProperties.hollowCandleStyle.drawWick": true,
+          "mainSeriesProperties.hollowCandleStyle.drawBorder": true,
+          "mainSeriesProperties.hollowCandleStyle.borderUpColor": "#32d777",
+          "mainSeriesProperties.hollowCandleStyle.borderDownColor": "#dc3545",
+          "mainSeriesProperties.hollowCandleStyle.wickUpColor": "#32d777",
+          "mainSeriesProperties.hollowCandleStyle.wickDownColor": "#dc3545",
+          "mainSeriesProperties.haStyle.upColor": "#32d777",
+          "mainSeriesProperties.haStyle.downColor": "#dc3545",
+          "mainSeriesProperties.haStyle.drawWick": true,
+          "mainSeriesProperties.haStyle.drawBorder": true,
+          "mainSeriesProperties.haStyle.borderUpColor": "#32d777",
+          "mainSeriesProperties.haStyle.borderDownColor": "#dc3545",
+          "mainSeriesProperties.haStyle.wickUpColor": "#32d777",
+          "mainSeriesProperties.haStyle.wickDownColor": "#dc3545",
+          "mainSeriesProperties.barStyle.upColor": "#32d777",
+          "mainSeriesProperties.barStyle.downColor": "#dc3545",
+          "mainSeriesProperties.barStyle.barColorsOnPrevClose": false,
+          "mainSeriesProperties.barStyle.dontDrawOpen": false,
+          "mainSeriesProperties.lineStyle.color": "#dc3545",
+        });
       });
-    });
-  };
-  //@ts-ignore
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPair: props.exchange.currentPair, // Set the initial state using the currentPair from props
     };
-    //@ts-ignore
-    this.ref = React.createRef();
-  }
 
-  componentDidMount() {
-    const widgetOptions = {
-      height: 480,
-      width: 1400,
+    chartInit(widgetOptions);
 
-      //@ts-ignore
-      symbol: `${this.state?.currentPair?.replace("_", "/")}`, // Use the currentPair from state
-      style: 1,
-      theme: theme === "dark" ? "dark" : "light",
-      //@ts-ignore
-      datafeed: Datafeed,
-      //@ts-ignore
-      interval: this.props.interval,
-      //@ts-ignore
-      container: this.ref.current,
-      //@ts-ignore
-      library_path: this.props.libraryPath,
-      //@ts-ignore
-      locale: getLanguageFromURL() || "en",
-      //@ts-ignore
-      charts_storage_url: this.props.chartsStorageUrl,
-      //@ts-ignore
-      charts_storage_api_version: this.props.chartsStorageApiVersion,
-      //@ts-ignore
-      client_id: this.props.clientId,
-      //@ts-ignore
-      user_id: this.props.userId,
-      //@ts-ignore
-      fullscreen: this.props.fullscreen,
-      //@ts-ignore
-      autosize: this.props.autosize,
-      //@ts-ignore
-      studies_overrides: this.props.studiesOverrides,
-      // drawings_access: { type: "black", tools: [{ name: "Regression Trend" }] },
-      //@ts-ignore
-      enabled_features: ENABLED_FEATURES,
-      //@ts-ignore
-      disabled_features: DISABLED_FEATURES,
-
-      //@ts-ignore
-      // overrides: getChartOverrides(this.props.theme),
-      custom_css_url: "css/style.css",
-      //@ts-ignore
-      time_frames: TIME_FRAMES,
-
-      //@ts-ignore
-      studies_overrides: {
-        "volume.volume.color.0": "#dc3545",
-        "volume.volume.color.1": "#32d777",
-        "volume.volume.transparency": 0,
-        "volume.volume ma.color": "#32d777",
-        "volume.volume ma.transparency": 0,
-        "volume.options.showStudyArguments": false,
-        "volume.options.showStudyTitles": false,
-        "volume.options.showStudyValues": false,
-        "volume.options.showLegend": false,
-        "volume.options.showStudyOutput": false,
-        "volume.options.showStudyOverlay": false,
-        "volume.options.showSeriesTitle": false,
-        "volume.options.showSeriesOHLC": false,
-        "volume.options.showBarChange": false,
-        "volume.options.showCountdown": false,
-      },
-      toolbar: false,
+    return () => {
+      if (tvWidget !== null) {
+        tvWidget.remove();
+        tvWidget = null;
+      }
     };
-    //@ts-ignore
-    this.chartInit(widgetOptions);
-  }
+  }, [coinpair]);
 
-  componentDidUpdate(prevProps: MyProps) {
-    console.log(prevProps.coinpair, "prevProps.coinpairprevProps.coinpair");
-    const widgetOptions = {
-      height: 480,
-      width: 1400,
+  return (
+    <>
+      <header className={styles.VersionHeader}></header>
+      <div ref={ref} className={styles.TVChartContainer} />
+    </>
+  );
+};
 
-      //@ts-ignore
-      symbol: `${this.state?.currentPair?.replace("_", "/")}`, // Use the currentPair from state
-      style: 1,
-      theme: theme === "dark" ? "dark" : "light",
-      //@ts-ignore
-      datafeed: Datafeed,
-      //@ts-ignore
-      interval: this.props.interval,
-      //@ts-ignore
-      container: this.ref.current,
-      //@ts-ignore
-      library_path: this.props.libraryPath,
-      //@ts-ignore
-      locale: getLanguageFromURL() || "en",
-      //@ts-ignore
-      charts_storage_url: this.props.chartsStorageUrl,
-      //@ts-ignore
-      charts_storage_api_version: this.props.chartsStorageApiVersion,
-      //@ts-ignore
-      client_id: this.props.clientId,
-      //@ts-ignore
-      user_id: this.props.userId,
-      //@ts-ignore
-      fullscreen: this.props.fullscreen,
-      //@ts-ignore
-      autosize: this.props.autosize,
-      //@ts-ignore
-      studies_overrides: this.props.studiesOverrides,
-      // drawings_access: { type: "black", tools: [{ name: "Regression Trend" }] },
-      //@ts-ignore
-      enabled_features: ENABLED_FEATURES,
-      //@ts-ignore
-      disabled_features: DISABLED_FEATURES,
 
-      //@ts-ignore
-      // overrides: getChartOverrides(this.props.theme),
-      custom_css_url: "css/style.css",
-      //@ts-ignore
-      time_frames: TIME_FRAMES,
-
-      //@ts-ignore
-      studies_overrides: {
-        "volume.volume.color.0": "#dc3545",
-        "volume.volume.color.1": "#32d777",
-        "volume.volume.transparency": 0,
-        "volume.volume ma.color": "#32d777",
-        "volume.volume ma.transparency": 0,
-        "volume.options.showStudyArguments": false,
-        "volume.options.showStudyTitles": false,
-        "volume.options.showStudyValues": false,
-        "volume.options.showLegend": false,
-        "volume.options.showStudyOutput": false,
-        "volume.options.showStudyOverlay": false,
-        "volume.options.showSeriesTitle": false,
-        "volume.options.showSeriesOHLC": false,
-        "volume.options.showBarChange": false,
-        "volume.options.showCountdown": false,
-      },
-      toolbar: false,
-    };
-    //@ts-ignore
-    this.chartInit(widgetOptions);
-    if (prevProps.coinpair !== this.props.coinpair) {
-      //@ts-ignore
-      this.setState({ currentPair: this.props.coinpair });
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.tvWidget !== null) {
-      //@ts-ignore
-      this.tvWidget.remove();
-      this.tvWidget = null;
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <header className={styles.VersionHeader}></header>
-        {/* @ts-ignore */}
-        <div ref={this.ref} className={styles.TVChartContainer} />
-      </>
-    );
-  }
-}
-
-const ConnectedTVChartContainer = connect(mapStateToProps)(TVChartContainer);
-
-export { ConnectedTVChartContainer as TVChartContainer };
