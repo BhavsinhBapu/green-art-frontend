6fa7b6f5c003cb8ea1777cfd323a2b0033da99f3
Tajuddin Molla <tajnwu@gmail.com>
2023-06-05 18:52:14 +0600

    Add Post Step Two  design update

--- components/P2P/AddPost/AddPostStep_two.tsx
+++ components/P2P/AddPost/AddPostStep_two.tsx
@@ -66,34 +66,36 @@ export const AddPostTwo = ({
     <div className="col-12 mt-4">
       <div className="buySellAddBox px-5 py-5 rounded">
         <div className="row">
-          <div className="col-md-6">
-            <label> Total Amount:</label>
-            <div className="P2psearchBox position-relative">
-              <input
-                type="number"
-                placeholder="0.00"
-                value={amount.amount}
-                onChange={(e) => {
-                  setAmount({
-                    ...amount,
-                    amount: e.target.value,
-                  });
-                }}
-              />
-              <button>
-                <span className="ml-3 text-muted">{selectedAsset?.value}</span>
-              </button>
-            </div>
-            <div className="adFromPriceInecDecButton mt-3">
-              <button className=" py-2" onClick={getAvailableBalanceAction}>
-                Get all balance
-              </button>
-            </div>
-          </div>
-
           <div className="col-12 mt-4">
             <div className="row">
-              <div className="col-md-6 col-lg-3">
+              <div className="col-6">
+                <label className="d-block">Payment Method</label>
+
+                <div className="col-12 p-0">
+                  <CUstomSelect
+                    options={PaymentOption}
+                    isSearchable={true}
+                    isMulti={true}
+                    placeholder="Add"
+                    handleFunction={handlePayment}
+                    defaultValue={selectedPayment}
+                  />
+                </div>
+                <small>Select up to 5 payment methods</small>
+              </div>
+              <div className="col-6">
+                <label>Payment Time Limit</label>
+                <div className="col-12 p-0">
+                  <CUstomSelect
+                    options={PaymentTime}
+                    isSearchable={false}
+                    placeholder="Select payment time"
+                    handleFunction={handlePaymentTime}
+                    defaultValue={selectedPaymentTime}
+                  />
+                </div>
+              </div>
+              <div className="col-6">
                 <label> Order Limit:</label>
                 <div className="P2psearchBox position-relative">
                   <input
@@ -115,7 +117,7 @@ export const AddPostTwo = ({
                   </button>
                 </div>
               </div>
-              <div className="col-md-6 col-lg-3">
+              <div className="col-6">
                 <label></label>
                 <div className="P2psearchBox position-relative mt-2">
                   <input
@@ -138,32 +140,30 @@ export const AddPostTwo = ({
                   </button>
                 </div>
               </div>
-              <div className="col-12 mt-4">
-                <label className="d-block pb-0 mb-0">Payment Method</label>
-                <span>Select up to 5 payment methods</span>
-                <div className="col-md-4 p-0">
-                  <CUstomSelect
-                    options={PaymentOption}
-                    isSearchable={true}
-                    isMulti={true}
-                    placeholder="Add"
-                    handleFunction={handlePayment}
-                    defaultValue={selectedPayment}
-                  />
-                </div>
-              </div>
-              <div className="col-12 mt-4">
-                <label>Payment Time Limit</label>
-                <div className="col-md-3 p-0">
-                  <CUstomSelect
-                    options={PaymentTime}
-                    isSearchable={false}
-                    placeholder="Select payment time"
-                    handleFunction={handlePaymentTime}
-                    defaultValue={selectedPaymentTime}
-                  />
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="col-6 mt-1">
+            <label> Total Amount:</label>
+            <div className="P2psearchBox position-relative">
+              <input
+                type="number"
+                placeholder="0.00"
+                value={amount.amount}
+                onChange={(e) => {
+                  setAmount({
+                    ...amount,
+                    amount: e.target.value,
+                  });
+                }}
+              />
+              <button>
+                <span className="ml-3 text-muted">{selectedAsset?.value}</span>
+              </button>
+            </div>
+            <div className="adFromPriceInecDecButton mt-3">
+              <button className=" py-2" onClick={getAvailableBalanceAction}>
+                Get all balance
+              </button>
             </div>
           </div>
         </div>
