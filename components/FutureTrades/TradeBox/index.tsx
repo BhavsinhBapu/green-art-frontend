f8f48d641d2f8aa55583b51ae5da486b086a4300
anirban00537 <anirban00537@gmail.com>
2023-06-19 11:21:36 +0600

    work in progress

--- components/FutureTrades/TradeBox/index.tsx
+++ components/FutureTrades/TradeBox/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const TradeBox = () => {
-  return <div>TradeBox</div>;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-8">8</div>
+        <div className="col-4">4</div>
+      </div>
+    </div>
+  );
 };
 
 export default TradeBox;
