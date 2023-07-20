d147fc325ecb08149d46572f279b185df03a842a
“Tajuddin <tajnwu@gmail.com>
2023-07-20 11:28:28 +0600

    Exchange Box Update

--- components/exchange/sell/stopLimit.tsx
+++ components/exchange/sell/stopLimit.tsx
@@ -30,7 +30,7 @@ const StopLimit = ({
     });
   };
   return (
-    <div id="BuyTabContent" className="tab-content">
+    <div id="BuyTabContent" className="tab-content p-0">
       <div
         id="imit"
         role="tabpanel"
