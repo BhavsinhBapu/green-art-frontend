55401a10e760483a6cc8b5d334f8de4e1e5af415
“Tajuddin <tajnwu@gmail.com>
2023-08-25 18:49:44 +0600

    Gift Card Reded Code See Update

--- service/gift-cards.ts
+++ service/gift-cards.ts
@@ -1,7 +1,7 @@
 import request from "lib/request";
 
 export const getGiftCardsData = async (ctxCookie: string) => {
-  const { data } = await request.get("/gift-card/gift-card-main-page",{
+  const { data } = await request.get("/gift-card/gift-card-main-page", {
     headers: {
       Authorization: `Bearer ${ctxCookie}`,
     },
@@ -9,86 +9,90 @@ export const getGiftCardsData = async (ctxCookie: string) => {
   return data;
 };
 
-
 export const getThemedGiftCardCategory = async () => {
   const { data } = await request.get(`/gift-card/gift-card-themes-page`);
   return data;
 };
 
-export const getThemedGiftCardDataApi = async (category : any,page: any, limit: any ) => {
+export const getThemedGiftCardDataApi = async (
+  category: any,
+  page: any,
+  limit: any
+) => {
   const { data } = await request.get(
     `/gift-card/get-gift-card-themes?uid=${category}&limit=${limit}&page=${page}`
   );
 
   return data;
-}
+};
 
 export const getMyCardPageDataApi = async () => {
   const { data } = await request.get(`/gift-card/my-gift-card-page`);
 
   return data;
-}
+};
 
-export const getMyCardsApi = async (status : any,limit: any, page: any ) => {
+export const getMyCardsApi = async (status: any, limit: any, page: any) => {
   const { data } = await request.get(
     `/gift-card/my-gift-card-list?status=${status}&limit=${limit}&page=${page}`
   );
 
   return data;
-}
+};
 
-export const handleCoinsApi = async(coin_type: any) => {
+export const handleCoinsApi = async (coin_type: any) => {
   const { data } = await request.get(
     `/gift-card/gift-card-wallet-data?coin_type=${coin_type}`
   );
 
   return data;
-} 
+};
 
 export const getBuyPageDataApi = async (uid: any) => {
   const { data } = await request.get(
     `/gift-card/buy-card-page-data?uid=${uid}`
   );
   return data;
-}
+};
 
-export const handleBuyCardApi = async(value : any) => {
+export const handleBuyCardApi = async (value: any) => {
   const { data } = await request.post(`/gift-card/buy-card`, {
     ...value,
   });
   return data;
-}
+};
 
-export const checkGiftCardApi = async (code : any) => {
+export const checkGiftCardApi = async (code: any) => {
   const { data } = await request.get(`gift-card/check-card?code=${code}`);
   return data;
-}
+};
 
-export const addGiftCardApi = async (code : any) => {
+export const addGiftCardApi = async (code: any) => {
   const { data } = await request.get(`gift-card/add-gift-card?code=${code}`);
   return data;
-}
+};
 
-export const redeemGiftCardApi = async (code : any) => {
+export const redeemGiftCardApi = async (code: any) => {
   const { data } = await request.get(`gift-card/redeem-card?code=${code}`);
   return data;
-}
+};
 
-export const sendGiftCardApi = async (url : any) => {
+export const sendGiftCardApi = async (url: any) => {
   const { data } = await request.get(url);
   return data;
-}
+};
 
-export const giftCardUpdateApi = async (buyDetails : any) => {
+export const giftCardUpdateApi = async (buyDetails: any) => {
   const { data } = await request.post(`/gift-card/update-card`, {
     ...buyDetails,
   });
   return data;
-}
+};
 
-export const getRedeemCodeApi = async (uid : any, pass: any) => {
-  const { data } = await request.get(
-    `/gift-card/get-redeem-code?card_uid=${uid}&password=${pass}`
-  );
+export const getRedeemCodeApi = async (uid: any, pass: any) => {
+  const { data } = await request.post(`/gift-card/get-redeem-code`, {
+    card_uid: uid,
+    password: pass,
+  });
   return data;
-}
+};
