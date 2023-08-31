ffaa7537cd249f129e171f7f28e6a5bde5250bc8
“Tajuddin <tajnwu@gmail.com>
2023-08-31 15:47:51 +0600

    Wallet Overview Update

--- components/wallet-overview/WalletOverviewHeader.tsx
+++ components/wallet-overview/WalletOverviewHeader.tsx
@@ -7,12 +7,12 @@ export default function WalletOverviewHeader({ title }: any) {
       <div className="profle-are-top">
         <h2>{title}</h2>
       </div>
-      <div>
+      <div className="wallet-overview-btn-all">
         <Link href={`/user/wallet-history?type=deposit`}>
           <a className="wallet-overview-btn">Deposit History</a>
         </Link>
         <Link href={`/user/wallet-history?type=withdrawal`}>
-          <a className="wallet-overview-btn mx-3">Withdrawal History</a>
+          <a className="wallet-overview-btn">Withdrawal History</a>
         </Link>
         <Link href={`/user/transaction-history`}>
           <a className="wallet-overview-btn">Transaction History</a>
