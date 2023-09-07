4cb42fad528d7e21a99d56cffbb62d2971c0c6c3
“Tajuddin <tajnwu@gmail.com>
2023-09-07 15:33:05 +0600

    Unauth Nav translation Add

--- components/common/unAuthNav.tsx
+++ components/common/unAuthNav.tsx
@@ -277,7 +277,7 @@ const UnAuthNav = ({
                         {isSettingsDropdownOpen && (
                           <div className="settings-dropdown">
                             <div className="settings-dropdown-header">
-                              <p>Theme</p>
+                              <p>{t("Theme")}</p>
                               <label className="gift-card-buy-switch mb-0">
                                 <input
                                   type="checkbox"
@@ -295,7 +295,7 @@ const UnAuthNav = ({
                             </div>
                             <div className="pb-3 border-bottom text-left">
                               <p className="mt-2 text-14 font-medium">
-                                Style Settings
+                                {t("Style Settings")}
                               </p>
                               <div className="form-check py-1">
                                 <input
@@ -322,7 +322,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex gap-5">
                                       <span className="margin-right-auto">
-                                        Fresh
+                                        {t("Fresh")}
                                       </span>
                                       <span
                                         className="settings-dropdown-color-box"
@@ -361,7 +361,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex gap-5">
                                       <span className="margin-right-auto text-14">
-                                        Traditional
+                                        {t("Traditional")}
                                       </span>
                                       <span
                                         className="settings-dropdown-color-box"
@@ -400,7 +400,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex gap-5">
                                       <span className="margin-right-auto">
-                                        Color Vision Deficiency
+                                        {t("Color Vision Deficiency")}
                                       </span>
                                       <span
                                         className="settings-dropdown-color-box"
@@ -417,7 +417,7 @@ const UnAuthNav = ({
                             </div>
                             <div className="pb-3 border-bottom text-left">
                               <p className="mt-2 text-14 font-medium">
-                                Color Preference
+                                {t("Color Preference")}
                               </p>
                               <div className="form-check py-1">
                                 <input
@@ -442,7 +442,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex">
                                       <span className="margin-right-auto">
-                                        Green Up/Red Down
+                                        {t("Green Up/Red Down")}
                                       </span>
                                       <span>
                                         <svg
@@ -505,7 +505,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex">
                                       <span className="margin-right-auto text-14">
-                                        Green Down/Red Up
+                                        {t("Green Down/Red Up")}
                                       </span>
                                       <span>
                                         <svg
@@ -570,7 +570,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex">
                                       <span className="margin-right-auto">
-                                        Layout one
+                                        {t("Layout one")}
                                       </span>
                                       <div>
                                         <img src="/layout_one.png" width={50} />
@@ -601,7 +601,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex">
                                       <span className="margin-right-auto text-14">
-                                        Layout two
+                                        {t("Layout two")}
                                       </span>
                                       <div>
                                         <img src="/layout_two.png" width={50} />
@@ -632,7 +632,7 @@ const UnAuthNav = ({
                                   <span className="w-full d-inline-block">
                                     <span className="d-flex">
                                       <span className="margin-right-auto text-14">
-                                        Layout three
+                                        {t("Layout three")}
                                       </span>
                                       <div>
                                         <img
@@ -842,7 +842,7 @@ const UnAuthNav = ({
                           <FiSettings size={16} />
                         </span>
                         <p className="text-16 text-primary-color-two line-h-19">
-                          Theme
+                          {t("Theme")}
                         </p>
                       </div>
                       <label className="gift-card-buy-switch mb-0">
