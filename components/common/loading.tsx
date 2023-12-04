091f6172d1676e82da3e2e11e5c38a53eda9c576
“Tajuddin <tajnwu@gmail.com>
2023-12-04 16:13:45 +0600

    Update

--- components/common/loading.tsx
+++ components/common/loading.tsx
@@ -14,20 +14,26 @@ const Loading = () => {
 
   return (
     <>
-      <div className="preloder-area">
-        {logo ? (
-          <span className="loading-image">
-            <img
-              src={logo || ""}
-              width={150}
-              className="img-fluid cp-user-logo-large"
-              alt=""
-            />
-          </span>
-        ) : (
+      {enableLogoAnimate == 2 ? (
+        <div className="preloder-area">
+          {logo ? (
+            <span className="loading-image">
+              <img
+                src={logo || ""}
+                width={150}
+                className="img-fluid cp-user-logo-large"
+                alt=""
+              />
+            </span>
+          ) : (
+            <span className="loader"></span>
+          )}
+        </div>
+      ) : (
+        <div className="preloder-area">
           <span className="loader"></span>
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
