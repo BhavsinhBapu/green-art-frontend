aea1dac2904c0fd282f9cbc25b939883de24ae12
“Tajuddin <tajnwu@gmail.com>
2024-01-24 18:45:57 +0600

    Update

--- components/FutureTrades/home/top-charts/TopCharts.tsx
+++ components/FutureTrades/home/top-charts/TopCharts.tsx
@@ -8,7 +8,7 @@ import ListCharts from "./ListCharts";
 
 export default function TopCharts({ tradeDatas }: any) {
   return (
-    <div className="row body-margin-top-custom">
+    <div className="row body-margin-top-custom" style={{ rowGap: "20px" }}>
       <div className="col-lg-3 col-md-6">
         <div className="shadow-sm wallet-card-info-container">
           <AreaCharts tradeDatas={tradeDatas} />
