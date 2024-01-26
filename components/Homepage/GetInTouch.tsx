92b59c325b24edf7ad3362c01df8eba05bbe5854
anirban_itech <anirban00537@gmail.com>
2024-01-26 15:23:50 +0600

    updated

--- components/Homepage/GetInTouch.tsx
+++ components/Homepage/GetInTouch.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import { motion } from "framer-motion";
-import PlaceBottomRight from "components/gradient/placeBottomRight";
-import PlaceTopLeft from "components/gradient/placeTopLeft";
+  import React from "react";
+  import { motion } from "framer-motion";
+  import PlaceBottomRight from "components/gradient/placeBottomRight";
+  import PlaceTopLeft from "components/gradient/placeTopLeft";
 
-const GetInTouch = ({ landing, featureListdata }: any) => {
-  return (
-    <div>
-      <PlaceTopLeft />
-      <PlaceBottomRight />
-      {parseInt(landing.landing_sixth_section_status) === 1 && (
-        <motion.section
-          className="get-touch-area"
-          style={{ padding: "60px 0" }}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-        >
-          <div className="container-4xl">
-            <div className="section-title mb-3">
-              <h2 className="title">{landing?.landing_feature_title}</h2>
-            </div>
-
-            <section className="hero-section">
-              <div className="card-grid">
-                {featureListdata?.map((feature: any, index: any) => (
-                  <Card key={index} feature={feature} />
-                ))}
+  const GetInTouch = ({ landing, featureListdata }: any) => {
+    return (
+      <div>
+        <PlaceTopLeft />
+        <PlaceBottomRight />
+        {parseInt(landing.landing_sixth_section_status) === 1 && (
+          <motion.section
+            className="get-touch-area"
+            style={{ padding: "60px 0" }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+          >
+            <div className="container-4xl">
+              <div className="section-title mb-3">
+                <h2 className="title">{landing?.landing_feature_title}</h2>
               </div>
-            </section>
-          </div>
-        </motion.section>
-      )}
-    </div>
-  );
-};
 
-export default GetInTouch;
-const Card = ({ feature }: any) => {
-  return (
-    <a className="card" href="#">
-      <div
-        className="card__background"
-        style={{
-          backgroundImage: `url(${feature.feature_icon})`,
-        }}
-      ></div>
-      <div className="card__content">
-        <h3 className="card__heading">{feature?.feature_title}</h3>
-        <p className="card__category">{feature?.description}</p>
+              <section className="hero-section">
+                <div className="card-grid">
+                  {featureListdata?.map((feature: any, index: any) => (
+                    <Card key={index} feature={feature} />
+                  ))}
+                </div>
+              </section>
+            </div>
+          </motion.section>
+        )}
       </div>
-    </a>
-  );
-};
+    );
+  };
+
+  export default GetInTouch;
+  const Card = ({ feature }: any) => {
+    return (
+      <a className="card glass-card" href="#">
+        <div
+          className="card__background"
+          style={{
+            backgroundImage: `url(${feature.feature_icon})`,
+          }}
+        ></div>
+        <div className="card__content">
+          <h3 className="card__heading">{feature?.feature_title}</h3>
+          <p className="card__category">{feature?.description}</p>
+        </div>
+      </a>
+    );
+  };
