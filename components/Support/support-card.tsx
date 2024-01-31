87bc209e827940368f3938d5e2f55b5b2e493e3b
“Tajuddin <tajnwu@gmail.com>
2024-01-31 12:48:40 +0600

    Update

--- components/Support/support-card.tsx
+++ components/Support/support-card.tsx
@@ -2,7 +2,7 @@ export const SupportCard = ({ name, ticketNumber, icon_name }: any) => {
   return (
     <div className="col-sm-6 col-12 col-lg-3 mt-3 mt-lg-0">
       <div className="h-100">
-        <div className="sub_title rounded">
+        <div className="sub_title">
           <div className="d-flex justify-content-center flex-column align-items-center py-3">
             <span className="card-top-icon mb-3">
               <i className={"fa " + icon_name} aria-hidden="true"></i>
