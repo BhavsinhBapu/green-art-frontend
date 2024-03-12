6fa4a202fb63f2ec14c6dabf365c73466d0bb31b
“Tajuddin <tajnwu@gmail.com>
2024-03-12 14:32:32 +0600

    Update

--- service/NextApi/helper.ts
+++ service/NextApi/helper.ts
@@ -5,4 +5,7 @@ export const apiErrorHandler = (error: any, res: any) => {
   if (error?.response?.status == 500) {
     res.status(500).json({ message: "Something Went Wrong", success: false });
   }
+  if (error?.response?.status == 404) {
+    res.status(404).json({ message: "Not Found", success: false });
+  }
 };
