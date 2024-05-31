d12dcd13d8075527c14ab453c55fd3a5a5e22f7e
ubuntu-hp <bacchu.cse11@gmail.com>
2024-05-31 17:12:26 +0600

    NEXT_PUBLIC_SECRET_KEY enabled again

--- lib/launchpadRequest.ts
+++ lib/launchpadRequest.ts
@@ -17,7 +17,7 @@ launchpadRequest.interceptors.request.use((config: any) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   config.headers.lang = lang ? lang : "en";
-  // config.headers.userapisecret = process.env.NEXT_PUBLIC_SECRET_KEY;
+  config.headers.userapisecret = process.env.NEXT_PUBLIC_SECRET_KEY;
   return config;
 });
 launchpadRequest.interceptors.response.use((response: any) => {
